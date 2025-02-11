d9503d49122b26f5f148a54f9c49947be60bd928
Brian Bautista <brianbauma10@gmail.com>
2025-02-11 16:02:51 +0100

    feat: :globe_with_meridians: Migrations: Usuario, Pelicula, Valoraciones, Comentarios, LikesComentarios, Lisas, Contenido, Notificaciones

---
+++ database/migrations/2025_01_27_202140_create_notificaciones_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notificaciones', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('id_usuario');
+            $table->text('mensaje');
+            $table->enum('tipo', ['nueva_temporada', 'nuevo_comentario', 'estreno']);
+            $table->boolean('leido')->default(false);
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notificaciones');
+    }
+};
