3f5cb277900b61bf51cc5c3fffd3dad0328d4126
Brian Bautista <brianbauma10@gmail.com>
2025-04-24 18:54:15 +0200

    feat: :adhesive_bandage: Footer Visual Update

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+    <h1 class="text-center mb-4">Series Populares</h1>
+
+    @if(count($series) > 0)
+        <div class="movie-grid">
+            @foreach($series as $serie)
+                <div class="movie-card">
+                    <div class="card-image">
+                        <img src="https://image.tmdb.org/t/p/w500{{ $serie['poster_path'] }}"
+                             alt="{{ $serie['name'] }}"
+                             onerror="this.src='{{ asset('images/default-poster.jpg') }}'">
+                    </div>
+                    <div class="card-info">
+                        <h3>{{ $serie['name'] }}</h3>
+                        <div class="card-meta">
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <span>{{ number_format($serie['vote_average'], 1) }}</span>
+                            </div>
+                            <span>{{ \Carbon\Carbon::parse($serie['first_air_date'])->format('Y') }}</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @else
+        <div class="empty-state">
+            <p>No se encontraron series disponibles.</p>
+        </div>
+    @endif
+</div>
+@endsection
