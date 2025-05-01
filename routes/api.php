22d4b75c33095e2cd7dcab10921726f3f8340a87
XCDavidXD2 <davidgustavo.correa@gracia.lasalle.cat>
2025-05-01 16:36:28 +0200

    feat: rutas endpoints para la api

--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,34 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::apiResource('contenido_listas', ContenidoListasController::class);
 Route::apiResource('usuarios', UsuariosController::class);
 Route::apiResource('peliculas_series', PeliculasSeriesController::class);
 Route::apiResource('valoraciones', ValoracionesController::class);
 Route::apiResource('comentarios', ComentariosController::class);
 Route::apiResource('likes_comentarios', LikesComentariosController::class);
 Route::apiResource('listas', ListasController::class);
+Route::apiResource('contenido_listas', ContenidoListasController::class);
 Route::apiResource('notificaciones', NotificacionesController::class);
 Route::apiResource('recomendaciones', RecomendacionesController::class);
 Route::apiResource('seguimientos', SeguimientoController::class);
 Route::apiResource('administradores', AdministradoresController::class);
 
-// Rutas para TMDB
-// Route::get('/tmdb/search', [PeliculasSeriesController::class, 'searchTMDB']);
-// Route::get('/tmdb/movie/{id}', [PeliculasSeriesController::class, 'getMovieDetails']);
+// Rutas adicionales para listas
+Route::get('listas/user/{userId}', [ListasController::class, 'getListasByUsuario']);
+Route::get('contenido_listas/lista/{id_lista}', [ContenidoListasController::class, 'getContenidoByListaId']);
+
+// Rutas para comentarios
+Route::get('comentarios/tmdb/{tmdbId}/{tipo}', [ComentariosController::class, 'getComentariosByTmdbId']);
+
+// Rutas para likes/dislikes de comentarios
+Route::get('likes_comentarios/status/{comentarioId}/{userId}', [LikesComentariosController::class, 'getLikeStatus']);
+Route::get('likes_comentarios/count/{comentarioId}', [LikesComentariosController::class, 'getLikesCount']);
+
+// Rutas para notificaciones
+Route::get('notificaciones/user/{userId}', [NotificacionesController::class, 'getUserNotificaciones']);
+Route::put('notificaciones/read/{id}', [NotificacionesController::class, 'markAsRead']);
+Route::put('notificaciones/read_all/{userId}', [NotificacionesController::class, 'markAllAsRead']);
+
+// Rutas para valoraciones (favoritos)
+Route::get('valoraciones/usuario/{userId}', [ValoracionesController::class, 'getUserFavorites']);
+Route::get('valoraciones/check/{userId}/{peliculaId}', [ValoracionesController::class, 'checkFavoriteStatus']);
